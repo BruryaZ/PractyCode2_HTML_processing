8b11031d260622e3fcb332bcb5355a7807cbb66c
Brurya-Zarbiv <bz0556758244@gmail.com>
2025-01-21 20:33:07 +0200

    after HTTP call before Breakdown by tags

---
+++ PROJECT2_HTMP_processing/HTMLElement.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROJECT2_HTMP_processing
+{
+    public class HTMLElement
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public List<string> Atributes { get; set; }
+        public List<string> Classes { get; set; }
+        public string InnerHTML { get; set; }
+
+        public HTMLElement Child { get; set; }
+        public HTMLElement Parent { get; set; }
+        public HTMLElement()
+        {
+        }
+
+        public HTMLElement(string id, string name, List<string> atributes, List<string> classes, string innerHTML, HTMLElement child, HTMLElement parent)
+        {
+            Id = id;
+            Name = name;
+            Atributes = atributes;
+            Classes = classes;
+            InnerHTML = innerHTML;
+            Child = child;
+            Parent = parent;
+        }
+    }
+}
---
+++ PROJECT2_HTMP_processing/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.RegularExpressions;
+
+static async Task<string> Load(string url)
+{
+    HttpClient client = new HttpClient();
+    var response = await client.GetAsync(url);
+    var html = await response.Content.ReadAsStringAsync();
+    return html;
+}
+
+var html = await Load("https://studio-chen.co.il/");
+
+// fillter the spaces
+var cleanHTML = new Regex("\\s").Replace(html, "");
+
+// separate the HTML to lines
+IEnumerable<string> HTMLlines = new Regex("<(.*?)>").Split(cleanHTML).Where(h => h.Length > 0);
+
+// separate the deatails
+
+IEnumerable<IEnumerable<string>> HTMLLis;
+foreach (var line in HTMLlines)
+    HTMLLis.Add(new Regex("([^\\s]*?)=\"(.*?)\"").Matches(HTMLlines));
+
+foreach (var line in HTMLlines)
+{
+    Console.WriteLine(line);
+}
+
+// custing to object
